be81b16fcb2ad1c3c62bfabab617063d05410f98
frenchy-mark <42653965+frenchy-mark@users.noreply.github.com>
2018-09-06 22:21:11 -0700

    Added checked out 4 weeks page

--- admin/admin.php
+++ admin/admin.php
@@ -7,6 +7,8 @@
 	<br>
 	<a href="checkedout.php">See all checked out books.</a>
 	<br>
+	<a href="checkedout4weeks.php">See all books checked out for 4 weeks or more.</a>
+	<br>
 	<a href="addbook.php">Add a book.</a>
 	<br>
 	<a href="deletebook.php">Delete a book.</a>
