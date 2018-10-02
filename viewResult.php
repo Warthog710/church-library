20815139a2d0e5bf4bc896db814731d800ca8143
Quinn Roemer <40554666+Warthog710@users.noreply.github.com>
2018-10-02 15:13:17 -0700

    Scaling issue fix
    Fixed the scaling issues with both checkDupe.php and viewResult.php. Also, re-wrote a section of returnbook.php to make it easier to understand.

--- viewResult.php
+++ viewResult.php
@@ -82,107 +82,7 @@
 	<br>
 
 
-	<div class="viewButtons" align="center">
-
-	 <button type="button" id="1" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="2" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="3" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="4" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="5" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="6" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="7" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="8" onclick="buttonClick(this.id)" hidden></button>
-	 <br> 
-	 <button type="button" id="9" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="10" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="11" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="12" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="13" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="14" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="15" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="16" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="17" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="18" onclick="buttonClick(this.id)" hidden></button> 
-	 <br>
-	 <button type="button" id="19" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="20" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="21" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="22" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="23" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="24" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="25" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="26" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="27" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="28" onclick="buttonClick(this.id)" hidden></button>
-	 <br> 
-	 <button type="button" id="29" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="30" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="31" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="32" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="33" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="34" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="35" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="36" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="37" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="38" onclick="buttonClick(this.id)" hidden></button> 
-	 <br>
-	 <button type="button" id="39" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="40" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="41" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="42" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="43" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="44" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="45" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="46" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="47" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="48" onclick="buttonClick(this.id)" hidden></button> 
-	 <br>
-	 <button type="button" id="49" onclick="buttonClick(this.id)" hidden></button>
-	 <br>
-	 <button type="button" id="50" onclick="buttonClick(this.id)" hidden></button>
+	<div id="viewButtons" align="center">
 	</div>
 
 	<br>
@@ -209,15 +109,26 @@
 			document.getElementById("result").innerHTML =  "Your search contained " + number + " results.";
 		}
 
-		for (var count = 1; count <= number; count++)
+		for (var count = 0; count < number; count++)
 		{
-			document.getElementById(count).removeAttribute("hidden");
-			document.getElementById(count).innerHTML =  row[count - 1]['title'] + ', by ' + row[count - 1]['first_name'] +  " " + row[count - 1]['last_name'] + ', from ' + row[count - 1]['publisher'];
-    }
+			//These variables are used to hold newly created variables.	
+			var button = document.createElement("button");
+			var text = document.createTextNode(row[count]['title'] + ', by ' + row[count]['first_name'] +  " " + row[count]['last_name'] + ', from ' + row[count]['publisher']);
+
+			//Setting the buttons attributes.
+			button.setAttribute('onclick', 'buttonClick(this.id)');
+			button.setAttribute('id', count);
+
+			//Appending the text to the button.
+			button.appendChild(text);
+
+			//Appending the button element to the viewButtons div.
+			document.getElementById("viewButtons").appendChild(button);
+   		}
 
 		function buttonClick(clicked_id)
 		{
-			var id = bookId[clicked_id - 1];
+			var id = bookId[clicked_id];
 
 			document.getElementById("input").value = id;
 
