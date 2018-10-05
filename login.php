50da8e7191780ce73668f1613152c06e0e198f41
Quinn Roemer <40554666+Warthog710@users.noreply.github.com>
2018-10-05 16:31:58 -0700

    Password hashing fix.
    Modified the way password and usernames are handled to be more secure.

--- login.php
+++ login.php
@@ -5,71 +5,79 @@
 </head>
 <body>
 
-	<input type="text" name="username" placeholder="Enter Username" id="username">
-	<input type="password" name="password" placeholder="Enter Password" id="password">
-	<button onclick="validateForm()">Submit</button>
+	<form action="login.php" method="post">
+  		<input type="text" name="username" placeholder="Enter Username" id="username">
+		<input type="password" name="password" placeholder="Enter Password" id="password">
+ 		<input type="submit" value="Submit">
+	</form> 
+
+	<p id="message" hidden>The password or username you entered was incorrect.</p>
 
 	<?php
 		include 'includes/dbh.php';
 
-		$sql = "SELECT * FROM admin;";
+		//------------------Mod Here------------------
 
-		$result = mysqli_query($conn, $sql);
+		/*$hash = password_hash("temp", PASSWORD_DEFAULT);
+		$sql = "UPDATE login SET password = '$hash';";		
+		mysqli_query($conn, $sql);
+		$sql = "UPDATE login SET username = '$hash';";
+		mysqli_query($conn, $sql);*/
 
-		$row = mysqli_fetch_assoc($result);
+		session_start();
+
+		//Checking to see if session exists.
+		if (isset($_SESSION['firstLoad']))
+		{
+			$sql = "SELECT * FROM login WHERE id=1;";
+			$result = mysqli_query($conn, $sql);
+			$row = mysqli_fetch_assoc($result);
+
+			//Grabbing posted variables if they exist.
+			if (isset($_POST['password']) && isset($_POST['username']))
+			{
+				$password = $_POST['password'];
+				$username = $_POST['username'];
+			}
+			else
+			{
+				//If the variables don't exist the code will exit.
+				exit;
+			}
+
+			//Verifying the password and username.
+			if(password_verify($password, $row['password']) && password_verify($username, $row['username']))
+			{
+				$goodLogin = true;
+				echo "is good";
+				$_SESSION['goodLogin'] = true;
+			}
+			else
+			{
+				$goodLogin = false;
+			}
+		}
+
+		//Setting the session variable after first load.
+		else
+		{
+			$_SESSION['firstLoad'] = 1;
+			$goodLogin = "firstLoad";
+		}		
 	?>
 
 
 	<script>
-		var row = <?php echo json_encode($row) ?>;
+		var goodLogin = <?php echo json_encode($goodLogin) ?>;	
 
-		var usernameHash = 0;
-		var passwordHash = 0;
-		var count = 0;
-
-		//alert (row['username'] + " " + row['pword']);
-			
-		function validateForm()
+		//If the password and username were correct...
+		if (goodLogin == true)
 		{
-			var password = document.getElementById("password").value;
-			var username = document.getElementById("username").value;
-
-			
-			if (password == null || password == "" || username == null || username == "")
-			{
-				alert("You must enter something in both password and username.")
-			}
-			else
-			{
-				for (count = 0; count < username.length; count++)
-				{
-					usernameHash = usernameHash + username.charCodeAt(count);
-					//console.log("1st loop");
-				}
-
-				for (count = 0; count < password.length; count++)
-				{
-					passwordHash = passwordHash + password.charCodeAt(count);
-					//console.log("2nd loop");
-				}
-
-				passwordHash = passwordHash * password.length;
-				usernameHash = usernameHash * username.length;
-
-				//console.log(usernameHash + " " + passwordHash);
-
-				if (passwordHash == row['pword'] && usernameHash == row['username'])
-				{
-					window.location='admin/admin.php';
-				}
-				else
-				{
-					usernameHash = 0;
-					passwordHash = 0;
-
-					alert("You entered the incorrect username or password. Please try again.");
-				}
-			}
+			window.location='admin/admin.php';
+		}
+		else
+		{
+			document.getElementById("message").removeAttribute("hidden");
 		}
 	</script>
 
